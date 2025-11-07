eae3efde357d51c6f23a61a2c7374fcee1738101
themillenniumfalcon <nishankpr2002@gmail.com>
2025-11-07 12:56:06 +0530

    added pybreaker

--- src/api/main.py
+++ src/api/main.py
@@ -1,19 +1,33 @@
 import uvicorn
+import pybreaker
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 
 app = FastAPI()
 
+breaker = pybreaker.CircuitBreaker(
+    fail_max=3,             # max failures before opening circuit
+    reset_timeout=10        # time (seconds) to wait before trying again (half-open)
+)
+
 class QueryRequest(BaseModel):
     query: str
+
+@breaker
+def get_agentic_response(request: QueryRequest):
+    try:
+        response = "Hello, World!"
+        return response
+    except:
+        raise HTTPException(status_code=504, detail="🔴 No response from downstream service (timeout or unreachable).")
 
 @app.post("/query")
 def query_endpoint(request: QueryRequest):
     try:
-        response = "Hello, World!"
+        response = get_agentic_response(request)
         return response
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
+    except pybreaker.CircuitBreakerError:
+        raise HTTPException(status_code=503, detail="🔴 Service temporarily unavailable (circuit open).")
         
 if __name__ == "__main__":
     uvicorn.run("src.api.main:app", host="127.0.0.1", port=6000, reload=True)
